6e2e74a4ca2cf9c6f91ba712175f14c73f742afc
Rex <htetnaing0814@gmail.com>
2026-08-31 12:41:59 +0700

    feedback component rename

--- lib/modules/feedback/feedback.dart
+++ lib/modules/feedback/feedback.dart
@@ -3,4 +3,4 @@
 export 'data/models/feedback.model.dart';
 export 'services/feedback.service.dart';
 export 'controllers/feedback.controller.dart';
-export 'views/feedback_bottom_sheet.dart';
+export 'components/feedback_bottom_sheet.dart';
